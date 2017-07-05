cf1df499181fd72e40b49d3b12be491ef70dca74
weics <weics.cn@gmail.com>
2017-07-05 21:12:01 +0800

    registry center

---
+++ rpc-registry/src/main/java/com/weics/rpc/registry/Constant.java
@@ -0,0 +1,14 @@
+package com.weics.rpc.registry;
+
+/**
+ * 常量的表示
+ * Created by weics on 2017/6/28.
+ */
+public class Constant {
+
+    public static final int ZK_SESSION_TIMEOUT = 5000;//zk的超时的时间
+
+    public static final String ZK_REGISTRY_PATH = "/registry";//注册的节点
+
+    public static final String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";//节点
+}
