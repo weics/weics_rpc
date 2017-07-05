e7b08ab14f32bc2b45946e07873fd3bab5dc4b8f
weics <weics.cn@gmail.com>
2017-07-05 21:17:18 +0800

    the example client of the customer rpc

---
+++ rpc-sample-client/src/main/java/com/weics/rpc/simple/client/HelloService.java
@@ -0,0 +1,12 @@
+package com.weics.rpc.simple.client;
+
+/**
+ * Created by weics on 2017/7/3.
+ */
+public interface HelloService {
+
+    String hello(String name);
+
+    String hello(Person person);
+
+}
